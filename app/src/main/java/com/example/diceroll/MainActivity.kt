d63fd4a2573db471de9a8831cb148bbb2ad3fda2
khukuh123 <khukuh.prihatmikho2@gmail.com>
2022-01-11 22:32:21 +0700

    day-2

--- app/src/main/java/com/example/diceroll/MainActivity.kt
+++ app/src/main/java/com/example/diceroll/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.diceroll
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.diceroll.databinding.ActivityMainBinding
@@ -8,6 +9,7 @@ import java.util.*
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var randomInt = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,12 +20,21 @@ class MainActivity : AppCompatActivity() {
         binding.rollButton.setOnClickListener {
             rollDice()
         }
+        binding.resultButton.setOnClickListener {
+            val intent = Intent(this, ResultActivity::class.java)
+            intent.putExtra("dice_result", randomInt)
+            startActivity(intent)
+        }
+        binding.about.setOnClickListener {
+            val intent = Intent(this, AboutActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private fun rollDice() {
 
 //        val randomInt = (1..6).random()
-        val randomInt = Random().nextInt(6) + 1
+        randomInt = Random().nextInt(6) + 1
         val drawableResource = when (randomInt){
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
